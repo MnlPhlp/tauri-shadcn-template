aceda6e98e03f0dbdd396fc5f18ce21a70067a5b
MnlPhlp <online@philipp-manuel.de>
2024-11-15 20:50:12 +0100

    working on ios and mac

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -11,7 +11,7 @@ pub fn run() {
         // Then register them (separated by a comma)
         .commands(tauri_specta::collect_commands![greet,]);
 
-    #[cfg(debug_assertions)] // <- Only export on non-release builds
+    #[cfg(all(debug_assertions,desktop))] // <- Only export on non-release builds
     builder
         .export(
             specta_typescript::Typescript::default(),
@@ -20,6 +20,7 @@ pub fn run() {
         .expect("Failed to export typescript bindings");
 
     tauri::Builder::default()
+        .plugin(tauri_plugin_shell::init())
         // and finally tell Tauri how to invoke them
         .invoke_handler(builder.invoke_handler())
         .setup(move |app| {
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import autoprefixer from "autoprefixer"
 import tailwind from "tailwindcss"
 import path from "node:path";;
 
-// @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
